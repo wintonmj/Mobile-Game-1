827e5fa62029d94261956a6cf48f902b201f0e9a
Michael Jay Winton <michaeljaywinton@Michaels-MacBook-Pro.local>
2025-04-02 00:52:51 +1300

    feat: enhance git hooks for better maintainability

---
+++ commitlint.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature
+        'fix',      // Bug fix
+        'docs',     // Documentation only changes
+        'style',    // Changes that do not affect the meaning of the code
+        'refactor', // Code change that neither fixes a bug nor adds a feature
+        'perf',     // Code change that improves performance
+        'test',     // Adding missing tests or correcting existing tests
+        'build',    // Changes that affect the build system or external dependencies
+        'ci',       // Changes to our CI configuration files and scripts
+        'chore',    // Other changes that don't modify src or test files
+        'revert'    // Reverts a previous commit
+      ]
+    ]
+  }
+}; 
